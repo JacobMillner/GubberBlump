80c4462a2f167b9a1c9c4fc6821270970019779c
Jacob Millner <jacob.millner@outlook.com>
2019-09-27 18:51:27 -0400

    adding more mouse randomness and an object detection threshhold

--- GubberBlump.py
+++ GubberBlump.py
@@ -32,22 +32,43 @@ while True:
         method = eval('cv.TM_CCOEFF_NORMED')
         # Apply template Matching
         res = cv.matchTemplate(img,template,method)
-        # debugging
-        #threshhold = 0.70
-        #loc = np.where( res >= threshhold)
-        #print("loc thresh: " + str(loc))
-        min_val, max_val, min_loc, max_loc = cv.minMaxLoc(res)
-        cur_pos = pyautogui.position()
-        bobber_pos = ((max_loc[0]+x_margin), (max_loc[1]+y_margin))
-        mouse = PyMouse()
-        mouseSpeed = 30
-        mouseCalc = MouseMovementCalculator(7, 5, mouseSpeed, 10*mouseSpeed)
-        coordsAndDelay = mouseCalc.calcCoordsAndDelay(cur_pos, bobber_pos)
-        for x, y, delay in coordsAndDelay:
-            mouse.move(round(x), round(y))
-            time.sleep(delay/1000)
-        print("Current Mouse Position: " + str(cur_pos))
-        print("min val: " + str(min_val))
-        print("max val: " + str(max_val))
-        print("min loc: " + str(min_loc))
-        print("max loc: " + str(max_loc))
+
+        # a threshhold to prevent false detection
+        threshhold = 0.65
+        loc = np.where( res >= threshhold)
+        if len(loc[0]) != 0:
+            min_val, max_val, min_loc, max_loc = cv.minMaxLoc(res)
+            cur_pos = pyautogui.position()
+            bobber_pos = ((max_loc[0]+x_margin), (max_loc[1]+y_margin))
+            mouse = PyMouse()
+            mouseSpeed = 19
+            coordsAndDelay = list()
+            mouseCalc = MouseMovementCalculator(7, 5, mouseSpeed, 10*mouseSpeed)
+
+            # set up some random crap to make mousemovement more variable
+            extra_movement = random.randint(0,100)
+            if extra_movement >= 75: # we accidently missed
+                print("and I oop...")
+                x_oops = random.randint(200,400)
+                y_oops = random.randint(200,400)
+                oops_sleep = (random.randint(200, 1500) / 1000)
+                oops_loc = bobber_pos = ((max_loc[0]+x_margin), (max_loc[1]+y_margin))
+                coordsAndDelay += mouseCalc.calcCoordsAndDelay(cur_pos, oops_loc)
+                time.sleep(oops_sleep)
+                coordsAndDelay += mouseCalc.calcCoordsAndDelay(oops_loc, bobber_pos)
+            else:
+                # just normal mouse move
+                coordsAndDelay += mouseCalc.calcCoordsAndDelay(cur_pos, bobber_pos)
+            
+            # fire 'er off!
+            for x, y, delay in coordsAndDelay:
+                mouse.move(round(x), round(y))
+                time.sleep(delay/1000)
+            print("Current Mouse Position: " + str(cur_pos))
+            print("min val: " + str(min_val))
+            print("max val: " + str(max_val))
+            print("min loc: " + str(min_loc))
+            print("max loc: " + str(max_loc))
+        else:
+            # we failed to detect the object
+            print("Mission failed!")
